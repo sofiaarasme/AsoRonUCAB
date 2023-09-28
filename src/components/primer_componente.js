2dfdf4c06d6e46610f54ad35bd8513a9495b2da4
sofiaarasme <111990944+sofiaarasme@users.noreply.github.com>
2023-09-28 15:01:54 -0400

    Mi primer commit

---
+++ src/components/primer_componente.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const primer_componente = () => {
+  return (
+    <div>
+        <h1>Mi primer primer_componente</h1>
+        <p>Esto es un parrafo en mi componente 1</p>
+    </div>
+  )
+}
