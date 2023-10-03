7d47822af8805a7ccf7ca9ede78bc3ac3246bc6c
sofiaarasme <111990944+sofiaarasme@users.noreply.github.com>
2023-10-02 20:34:05 -0400

    Menu Admin, con iconos y hover para el nombre.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Login } from './views/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/views/Admin_Menu.js
+++ src/views/Admin_Menu.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import React from 'react';
+import './Admin_Menu.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// Importa los iconos de FontAwesome que necesitas
+import { faBriefcase, faGlobe, faLayerGroup, faEarthAmericas, faFaceSmile, faFileInvoice, faCalendarDays, faAddressBook, faNewspaper, faTruckFast, faCartShopping, faUsers, faCog } from '@fortawesome/free-solid-svg-icons';
+
+// Agrega los iconos al library de FontAwesome
+library.add(faBriefcase, faGlobe, faLayerGroup, faEarthAmericas, faFaceSmile, faFileInvoice, faCalendarDays, faAddressBook, faNewspaper, faTruckFast, faCartShopping, faUsers, faCog);
 
 export const Admin_Menu = () => {
+  let botones = [
+    { icono: "briefcase", nombre: "Ventas" },
+    { icono: "globe", nombre: "Sitio Web" },
+    { icono: "layer-group", nombre: "Inventario" },
+    { icono: "face-smile", nombre: "Clientes" },
+    { icono: "file-invoice", nombre: "Facturación" },
+    { icono: "calendar-days", nombre: "Eventos" },
+    { icono: "address-book", nombre: "Nómina" },
+    { icono: "newspaper", nombre: "Diario" },
+    { icono: "truck-fast", nombre: "Distribución" },
+    { icono: "cart-shopping", nombre: "Compras" },
+    { icono: "users", nombre: "Usuarios" },
+    { icono: "cog", nombre: "Otros" }
+  ];
+
+  // Colores para los botones
+  const colores = [
+    "#007bff",
+    "#ff5733",
+    "#33ff57",
+    "#5733ff",
+    "#ff3357",
+    "#33ff99",
+    "#ff9933",
+    "#33ffff",
+    "#ff66b2",
+    "#9933ff",
+    "#33cc33",
+    "#ffcc33"
+  ];
+
   return (
-    <div>Admin_Menu</div>
-  )
-}
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="button-container">
+            {botones.map((boton, index) => {
+              const estiloBoton = {
+                backgroundColor: colores[index % colores.length],
+              };
+              return (
+                <div className="col-md-3 d-flex justify-content-center align-items-center" key={index}>
+                  <button className="square-button" style={estiloBoton}>
+                    <FontAwesomeIcon icon={boton.icono} />
+                    <span className="button-name">{boton.nombre}</span>
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
